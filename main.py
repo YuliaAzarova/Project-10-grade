4de4d61977101610e7f58b96c5331ecc3b537660
YuliaAzarova <67875047+YuliaAzarova@users.noreply.github.com>
2026-02-17 21:43:07 +0300

    update main.py

--- main.py
+++ main.py
@@ -7,130 +7,16 @@ import sorts
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.spinner import Spinner
-from pygments.styles.dracula import background
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.graphics import Rectangle, Color
-from kivy.animation import Animation
-from kivy.clock import Clock
-
-
-class BarsWidget(Widget):
-    def __init__(self, values, **kwargs):
-        super().__init__(**kwargs)
-        self.values = values
-        self.original_values = values.copy()
-        self.bars = []
-        self.history = []
-
-        self.bar_width = 70
-        self.spacing = 10
-        self.max_value = max(values)
-
-        self.bind(size=self.draw_bars)
-
-        self.anim_event = None
-        self.animating = False
-
-    def draw_bars(self, *args):
-        self.canvas.clear()
-        self.bars.clear()
-
-        height_scale = (self.height) / self.max_value * 1.4
-        total_width = (len(self.values) * self.bar_width +
-                       (len(self.values) - 1) * self.spacing)
-        start_x = self.x + (self.width - total_width) / 2
-        self.y = 430
-
-        with self.canvas:
-            for i, value in enumerate(self.values):
-                color = Color(1, .5, 0, 1)
-                rect = Rectangle(
-                    pos=(start_x + i * (self.bar_width + self.spacing), self.y),
-                    size=(self.bar_width, value * height_scale)
-                )
-                self.bars.append({
-                    "rect": rect,
-                    "color": color
-                })
-
-    def step(self, swap):
-        if not swap:
-            return
-        i, j = swap
-        self.animation(i, j)
-
-    def reset_colors(self, i, j):
-        if i < len(self.bars) and j < len(self.bars):
-            self.bars[i]["color"].rgba = (1, .5, 0, 1)
-            self.bars[j]["color"].rgba = (1, .5, 0, 1)
-
-    def animation(self, i, j, duration=0.27):
-        bar1 = self.bars[i]
-        bar2 = self.bars[j]
-
-        bar1["color"].rgba = (1, 0.3, 0.3, 1)
-        bar2["color"].rgba = (1, 0.3, 0.3, 1)
-
-        rect1 = bar1["rect"]
-        rect2 = bar2["rect"]
-
-        x1 = rect1.pos[0]
-        x2 = rect2.pos[0]
-
-        y1 = rect1.pos[1]
-        y2 = rect2.pos[1]
-
-        anim1 = Animation(pos=(x2, y1), duration=duration)
-        anim2 = Animation(pos=(x1, y2), duration=duration)
-
-        anim1.start(rect1)
-        anim2.start(rect2)
-
-        Clock.schedule_once(
-        lambda dt: self.reset_colors(i, j),
-            0.3)
-
-        self.bars[i], self.bars[j] = self.bars[j], self.bars[i]
-        self.values[i], self.values[j] = self.values[j], self.values[i]
-        Clock.schedule_once(lambda dt: setattr(self, "busy", False), duration)
-        self.busy = True
-
-    def animate(self, swaps, index=0):
-        if not self.animating:
-            return
-
-        if index == len(swaps):
-            return
-
-        i, j = swaps[index]
-        self.animation(i, j)
-
-        self.anim_event = Clock.schedule_once(
-            lambda dt: self.animate(swaps, index + 1),
-            0.3
-        )
-
-    def reset(self):
-        self.animating = False
-
-        if self.anim_event:
-            self.anim_event.cancel()
-            self.anim_event = None
-
-        for rect in self.bars:
-            Animation.cancel_all(rect["rect"])
-        self.values = self.original_values.copy()
-        self.draw_bars()
+from widget import BarsWidget
 
 
 class BarGraphApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
 
-        # выбор сортировки
         self.spinner = Spinner(
             text='Сортировка пузырьком',
             values=('Сортировка пузырьком', 'Сортировка слиянием', 'Сортировка вставками'),
@@ -155,7 +41,7 @@ class BarGraphApp(App):
         self.bars_widget = BarsWidget(self.data)
         layout.add_widget(self.bars_widget)
 
-        # кнопки
+
         self.scroll_view = ScrollView(size_hint=(1, 1))
         self.buttons_container = BoxLayout(orientation='vertical', spacing=5, size_hint_y=0.8)
 
@@ -181,9 +67,9 @@ class BarGraphApp(App):
 
 
 
-        self.animation_steps = [] # шаги запоминать
-        self.anim_index = 0 # считать шаги
-        self.anim_event = None # чекать
+        self.animation_steps = []
+        self.anim_index = 0
+        self.anim_event = None
 
 
 
@@ -193,14 +79,17 @@ class BarGraphApp(App):
 
     def on_spinner_select(self, spinner, value):
         self.status_label.text = value
-# +
 
 
 
     def on_press_sort(self, instance):
         if instance.text == "Сбросить сортировку":
             self.bars_widget.reset()
-            self.anim_index = 0
+            self.anim_index *= 0
+            if self.status_label.text == "Сортировка пузырьком":
+                self.animation_steps = sorts.bubble_sort_steps(self.bars_widget.original_values.copy())
+            elif self.status_label.text == "Сортировка вставками":
+                self.animation_steps = sorts.insert_sort_steps(self.bars_widget.original_values.copy())
 
             instance.text = "Запустить сортировку"
 
@@ -210,15 +99,20 @@ class BarGraphApp(App):
                 self.st_back.disabled = True
             return
 
-
         self.bars_widget.animating = True
 
         if self.status_label.text == "Сортировка пузырьком":
             self.bars_widget.animating = True
-            self.animation_steps = sorts.bubble_sort_steps(self.bars_widget.values.copy())
+            if self.anim_index > 0:
+                self.animation_steps = sorts.bubble_sort_steps(self.bars_widget.values.copy())
+            else:
+                self.animation_steps = sorts.bubble_sort_steps(self.bars_widget.original_values.copy())
         elif self.status_label.text == "Сортировка вставками":
             self.bars_widget.animating = True
-            self.animation_steps = sorts.insert_sort_steps(self.bars_widget.values.copy())
+            if self.anim_index > 0:
+                self.animation_steps = sorts.insert_sort_steps(self.bars_widget.values.copy())
+            else:
+                self.animation_steps = sorts.insert_sort_steps(self.bars_widget.original_values.copy())
         elif self.status_label.text == "Сортировка слиянием":
             pass
 
@@ -230,7 +124,8 @@ class BarGraphApp(App):
 
 
     def on_press_steps(self, instance):
-        self.anim_index = 0
+        self.bars_widget.reset()
+        self.anim_index *= 0
         self.bars_widget.animating = False
         self.st_back = Button(text='Шаг назад',
                             font_size='15sp',
@@ -244,13 +139,15 @@ class BarGraphApp(App):
                                 height=50,
                             on_press=self.on_press_s_forward,
                                     background_color="#4169E1")
-        if self.button_sort.text == "Сбросить сортировку":
-            self.st_forward.disabled= True
+        if self.anim_index > 0:
+            self.st_forward.disabled= False
             self.st_back.disabled = False
+        else:
+            self.st_forward.disabled = False
+            self.st_back.disabled = True
 
 
-        button_layout = BoxLayout(orientation='horizontal',
-                                      spacing=20)
+        button_layout = BoxLayout(orientation='horizontal', spacing=20)
 
         button_layout.add_widget(self.st_back)
         button_layout.add_widget(self.st_forward)
@@ -259,11 +156,11 @@ class BarGraphApp(App):
         instance.text = "Идет сортировка"
         instance.disabled = True
 
-        self.anim_index = 0
+        self.anim_index *= 0
         if self.status_label.text == "Сортировка пузырьком":
-            self.animation_steps = sorts.bubble_sort_steps(self.bars_widget.values.copy())
+            self.animation_steps = sorts.bubble_sort_steps(self.bars_widget.original_values.copy())
         elif self.status_label.text == "Сортировка вставками":
-            self.animation_steps = sorts.insert_sort_steps(self.bars_widget.values.copy())
+            self.animation_steps = sorts.insert_sort_steps(self.bars_widget.original_values.copy())
 
 
 
@@ -291,16 +188,14 @@ class BarGraphApp(App):
 
         self.st_back.disabled = False
         swap = self.animation_steps[self.anim_index]
-        self.anim_index += 1
         self.bars_widget.step(swap)
+        self.anim_index += 1
 
         if self.anim_index >= len(self.animation_steps):
             instance.disabled = True
             self.button_sort.text = "Сбросить сортировку"
+            self.st_back.disabled = True
             return
-
-def smth():
-    pass
 
 
 if __name__ == '__main__':
